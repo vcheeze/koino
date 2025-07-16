8a95351797c7807d6abec64e5807622fbe9a63f2
Peter Chen <manchesterutd7@gmail.com>
2025-07-16 17:48:40 +0400

    chore: update seo and remove unnecessary files

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 import type { QueryClient } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {
-	createRootRouteWithContext,
-	HeadContent,
-	Outlet,
-	Scripts,
+  createRootRouteWithContext,
+  HeadContent,
+  Outlet,
+  Scripts,
 } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import type * as React from 'react'
@@ -18,79 +18,78 @@ import appCss from '~/styles/app.css?url'
 import { seo } from '~/utils/seo'
 
 export const Route = createRootRouteWithContext<{
-	queryClient: QueryClient
+  queryClient: QueryClient
 }>()({
-	head: () => ({
-		meta: [
-			{
-				charSet: 'utf-8',
-			},
-			{
-				name: 'viewport',
-				content: 'width=device-width, initial-scale=1',
-			},
-			...seo({
-				title:
-					'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
-				description:
-					'TanStack Start is a type-safe, client-first, full-stack React framework. ',
-			}),
-		],
-		links: [
-			{ rel: 'stylesheet', href: appCss },
-			{
-				rel: 'apple-touch-icon',
-				sizes: '180x180',
-				href: '/apple-touch-icon.png',
-			},
-			{
-				rel: 'icon',
-				type: 'image/png',
-				sizes: '32x32',
-				href: '/favicon-32x32.png',
-			},
-			{
-				rel: 'icon',
-				type: 'image/png',
-				sizes: '16x16',
-				href: '/favicon-16x16.png',
-			},
-			{ rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-			{ rel: 'icon', href: '/favicon.ico' },
-		],
-	}),
-	errorComponent: (props) => {
-		return (
-			<RootDocument>
-				<DefaultCatchBoundary {...props} />
-			</RootDocument>
-		)
-	},
-	notFoundComponent: () => <NotFound />,
-	component: RootComponent,
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      ...seo({
+        title: 'Koinó | Fellowship through Prayer',
+        description:
+          'A prayer tracking app designed for churches to encourage daily prayer through member directories. Build habits of intercession and unity.',
+      }),
+    ],
+    links: [
+      { rel: 'stylesheet', href: appCss },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        sizes: '32x32',
+        href: '/logo.svg',
+      },
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        sizes: '16x16',
+        href: '/logo.svg',
+      },
+      { rel: 'manifest', href: '/site.webmanifest', color: '#f6f2f1' },
+      { rel: 'icon', href: '/logo.svg' },
+    ],
+  }),
+  errorComponent: (props) => {
+    return (
+      <RootDocument>
+        <DefaultCatchBoundary {...props} />
+      </RootDocument>
+    )
+  },
+  notFoundComponent: () => <NotFound />,
+  component: RootComponent,
 })
 
 function RootComponent() {
-	return (
-		<RootDocument>
-			<Outlet />
-		</RootDocument>
-	)
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<HeadContent />
-			</head>
-			<body className="font-serif">
-				<ScrollArea className="h-screen">{children}</ScrollArea>
-				<Toaster />
-				<ReactQueryDevtools buttonPosition="bottom-left" />
-				<TanStackRouterDevtools position="bottom-right" />
-				<Scripts />
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body className="font-serif">
+        <ScrollArea className="h-screen">{children}</ScrollArea>
+        <Toaster />
+        <ReactQueryDevtools buttonPosition="bottom-left" />
+        <TanStackRouterDevtools position="bottom-right" />
+        <Scripts />
+      </body>
+    </html>
+  )
 }
